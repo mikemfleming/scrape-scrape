73939450a1d74296a85e1644cb4ed3f9a1623a8d
Michael Fleming <flemingatx@gmail.com>
2016-09-20 19:10:36 -0500

    scrapes, adds output.json to gitignore

--- server.js
+++ server.js
@@ -5,8 +5,47 @@ var cheerio = require('cheerio');
 var app 	= express();
 
 app.get('/scrape', function(req, res){
+	var url = 'http://www.imdb.com/title/tt1229340/';
 
+	request(url, function(error, response, html){
+		if (!error){
+			var $ = cheerio.load(html);
+
+			var title, release, rating;
+
+			var json = { title: "", release: "", rating: ""}
+
+			$('.title_wrapper').filter(function(){
+				var data = $(this)
+				title = data.children().first().text();
+				json.title = title
+			})
+
+			$('#titleYear').filter(function(){
+				var data = $(this)
+				release = data.text()
+				json.release = release
+			})
+
+			$('.star-box-giga-star').filter(function(){
+				var data = $(this)
+				rating = data.text()
+				json.rating = rating
+			})
+		}
+		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+			if (err){
+				console.log('~~~~', err)
+			} else {
+				console.log('success!')
+			}
+		})
+	})
+
+
+	res.send('Check yer console!')
 })
+
 
 app.listen('8081')
 
